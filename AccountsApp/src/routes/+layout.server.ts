8cc433c9904e16df78bcbb783aaed93022d3f032
Dominick Caponi <d.caponi1@gmail.com>
2023-09-02 09:39:37 -0700

    documentation

--- AccountsApp/src/routes/+layout.server.ts
+++ AccountsApp/src/routes/+layout.server.ts
@@ -1,14 +1,10 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
-export type OutputType = { isLoggedIn: boolean };
 
-export const load: LayoutServerLoad<OutputType> = async ({ locals, url }) => {
+export const load: LayoutServerLoad = async ({ locals, url }) => {
     const protectedRoutes = ['/protected']
 
     if (!locals.pb?.authStore.isValid && protectedRoutes.includes(url.pathname)) {
         throw redirect(302, '/login')
     }
-    return {
-            isLoggedIn: locals.pb?.authStore.isValid ? true : false,
-    };
 };
