5be716330f3129039d1a36a5779b2f62cdce6e55
Dominick Caponi <d.caponi1@gmail.com>
2025-01-17 22:39:18 -0800

    adds node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,22 @@
-import vercel from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+//import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-node";
+
+import compression from "compression";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		adapter: vercel()
-	}
+  kit: {
+    adapter: adapter({
+      // Add the compression middleware to the adapter options
+      middleware: (handler) => compression()(handler),
+      worker: true, // Add this line if the adapter supports handling worker files
+    }),
+  },
+  preprocess: vitePreprocess(),
+  paths: {
+    relative: false,
+  },
 };
 
 export default config;
